d4ef93422885afea864a1b56271b07d1d76d06cb
Jon Palmer <328224+jonspalmer@users.noreply.github.com>
2020-04-06 18:58:17 -0400

    Try alias_chain monkey patch

--- lib/view_component/render_monkey_patch.rb
+++ lib/view_component/render_monkey_patch.rb
@@ -2,11 +2,29 @@
 
 module ViewComponent
   module RenderMonkeyPatch # :nodoc:
-    def render(options = {}, args = {}, &block)
+    def self.included(base)
+      # Protect from trying to augment modules that appear
+      # as the result of adding other gems.
+      return if base != ActionController::Base
+
+      base.class_eval do
+        alias_method_chain :render, :view_component
+      end
+    end
+
+    def render(options = nil, *args, &block)
+      render_with_view_component(options, *args, &block)
+    end
+
+    def render_with_view_component(options = nil, *args, &block)
       if options.respond_to?(:render_in)
         options.render_in(self, &block)
+      elsif respond_to?(:render_without_view_component)
+        # support alias_method_chain (module included)
+        render_without_view_component(options, *args, &block)
       else
-        super
+        # support inheritance (module prepended)
+        method(:render).super_method.call(options, *args, &block)
       end
     end
   end
--- lib/view_component/rendering_monkey_patch.rb
+++ lib/view_component/rendering_monkey_patch.rb
@@ -2,11 +2,29 @@
 
 module ViewComponent
   module RenderingMonkeyPatch # :nodoc:
+    def self.included(base)
+      # Protect from trying to augment modules that appear
+      # as the result of adding other gems.
+      return if base != ActionController::Base
+
+      base.class_eval do
+        alias_method_chain :render, :view_component
+      end
+    end
+
     def render(options = {}, args = {})
+      render_with_view_component(options, args)
+    end
+
+    def render_with_view_component(options = {}, args = {})
       if options.respond_to?(:render_in)
         self.response_body = options.render_in(self.view_context)
+      elsif respond_to?(:render_without_view_component)
+        # support alias_method_chain (module included)
+        render_without_view_component(options, args)
       else
-        super
+        # support inheritance (module prepended)
+        method(:render).super_method.call(options, args)
       end
     end
   end
